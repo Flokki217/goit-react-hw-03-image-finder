b04518ff2ae38b7ce4034659bf7309f8490e1ca7
Flokki217 <flokki217@gmail.com>
2023-09-17 20:37:04 +0200

    last of us

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,33 @@
+import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
+import React, { Component } from 'react';
+import css from './ImageGallery.module.css';
+class ImageGallery extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    const { searchQuery, page, renderImages } = this.props;
+    if (prevProps.searchQuery !== searchQuery) {
+      renderImages(searchQuery, page);
+    }
+  }
+
+  render() {
+    const { images, onImageClick } = this.props;
+
+    return (
+      <div className={css.wrapper}>
+        <ul className={css.gallery}>
+          {images.map(({ id, webformatURL, largeImageURL }) => (
+            <ImageGalleryItem
+              className={css.image}
+              key={id}
+              imageUrl={webformatURL}
+              alt={`Image ${id}`}
+              onImageClick={() => onImageClick(largeImageURL)}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default ImageGallery;
